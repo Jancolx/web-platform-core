730dc5846d2dfed626065e1037135cb7e7b5aa1b
bsray <bs@gmail.com>
2026-04-14 19:35:07 +0530

    dssd

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,3 +10,18 @@ variable "image_tag" {
   type    = string
   default = "latest"
 }
+
+data "aws_ami" "latest_linux" {
+  most_recent = true
+  owners      = ["amazon"] # Official Amazon images
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
